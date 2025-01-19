06ca8cbf883ffd20731a2023d2ee78e5b216d56e
hiananya02@gmail.com <hiananya02@gmail.com>
2025-01-19 20:29:42 +0530

    build done

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "topsis-ANANYA-102216114=topsis-ANANYA-102216114:main",  
+            "topsis-ANANYA-102216114=102216114:main",  
         ],
     },
     classifiers=[
